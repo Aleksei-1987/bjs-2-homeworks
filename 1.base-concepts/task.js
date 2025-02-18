3c736fa312ee6cc8117a0a61183e8705c4d2bb4b
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-02-18 21:02:01 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		arr.push(-b / (2 * a));
+	} else if (discriminant > 0) {
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a));
+	}
+
+	return arr;
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthPercentСoefficient = percent / 100 / 12;
+	let creditBody = amount - contribution;
+	let monthlyPayment = creditBody * (monthPercentСoefficient + monthPercentСoefficient / (Math.pow((1 + monthPercentСoefficient), countMonths) - 1));
+	let totalAmount = monthlyPayment * countMonths;
+	return +totalAmount.toFixed(2);
 }
+
