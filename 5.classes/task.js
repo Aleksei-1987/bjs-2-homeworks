dff361684d154b4a329f27142ef397eaf46afe41
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-03-05 21:21:03 +0300

    Задание 5

---
+++ 5.classes/task.js
@@ -0,0 +1,131 @@
+//Задача 1
+
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state = this.state * 1.5;
+	}
+
+	set state(number) {
+		if (number < 0) {
+			this._state = 0;
+		} else if (number > 100) {
+			this._state = 100
+		} else {
+			this._state = number;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "detective";
+	}
+}
+
+// Задача 2
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		return this.books.find(book => book[type] === value) || null;
+	}
+
+	giveBookByName(bookName) {
+		const findBookname = this.books.find(book => book.name === bookName);
+		if (!findBookname) {
+			return null;
+		}
+		return this.books.pop();
+	}
+}
+
+// Задача 3
+
+class Student {
+	constructor(name, gender, age) {
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.marks = {};
+	}
+
+	addMark(mark, subject) {
+		if (2 > mark || mark > 5) {
+			return
+		}
+		if (!this.marks.hasOwnProperty(subject)) {
+			this.marks[subject] = [];
+		}
+		this.marks[subject].push(mark);
+	}
+
+	getAverageBySubject(subject) {
+		if (!this.marks.hasOwnProperty(subject)) {
+			return 0;
+		}
+
+		return this.marks[subject].reduce((a, v) => a + v, 0) / this.marks[subject].length;
+	}
+
+	getAverage() {
+		const subjects = Object.keys(this.marks);
+		if (subjects.length === 0) {
+			return 0;
+		}
+
+		return subjects.reduce((total, subject) => total + this.getAverageBySubject(subject), 0) / subjects.length;
+	}
+}
